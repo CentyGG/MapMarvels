184fb272cf36e56fb9667267d435c5bb2e61f057
DenisVikoruk <dd.vikoruk@gmail.com>
2024-04-20 12:34:07 +0300

    1

--- app/src/main/java/com/example/mapmarvels/CameraFragment.java
+++ app/src/main/java/com/example/mapmarvels/CameraFragment.java
@@ -1,64 +1,96 @@
 package com.example.mapmarvels;
-
 import android.os.Bundle;
-
+import androidx.camera.core.Camera;
 import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CameraFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContentValuesKt;
+import androidx.core.content.ContextCompat;
+import android.os.Environment;
+import android.widget.Toast;
+import com.google.common.util.concurrent.ListenableFuture;
+import java.io.File;
+import java.util.concurrent.ExecutionException;
 public class CameraFragment extends Fragment {
+    public class CameraActivity extends AppCompatActivity {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+        private PreviewView previewView;
+        private ImageCapture imageCapture;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.fragment_camera);
 
-    public CameraFragment() {
-        // Required empty public constructor
-    }
+            previewView = findViewById(R.id.pv_camera);
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CameraFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CameraFragment newInstance(String param1, String param2) {
-        CameraFragment fragment = new CameraFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+            ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            cameraProviderFuture.addListener(() -> {
+                try {
+                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+
+                    bindPreview(cameraProvider);
+
+                    imageCapture = new ImageCapture.Builder().build();
+
+                    findViewById(R.id.bottom_bar).setOnClickListener(v -> takePhoto());
+                } catch (ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }, ContextCompat.getMainExecutor(this));
         }
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false);
+        private void bindPreview(ProcessCameraProvider cameraProvider) {
+            Preview preview = new Preview.Builder().build();
+
+            CameraSelector cameraSelector = new CameraSelector.Builder()
+                    .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                    .build();
+
+            preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+            Camera camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview);
+        }
+
+        private void takePhoto() {
+            File outputDirectory = getOutputDirectory();
+            ImageCapture.OutputFileOptions outputFileOptions =
+                    new ImageCapture.OutputFileOptions.Builder(
+                            new File(outputDirectory, "photo.jpg")).build();
+
+            imageCapture.takePicture(outputFileOptions, ContextCompat.getMainExecutor(this),
+                    new ImageCapture.OnImageSavedCallback() {
+                        @Override
+                        public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                            Toast.makeText(CameraActivity.this, "Photo captured!", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onError(@NonNull ImageCaptureException exception) {
+                            exception.printStackTrace();
+                        }
+                    });
+        }
+
+        private File getOutputDirectory() {
+            File mediaDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+            File outputDir = new File(mediaDir, "CameraXApp");
+
+            if (!outputDir.exists()) {
+                outputDir.mkdirs();
+            }
+
+            return outputDir;
+        }
+
+        public void finish() {
+        }
     }
 }
